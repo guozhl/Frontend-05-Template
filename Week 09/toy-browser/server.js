c9f9e263a7872a93f459e05874744fb04e9e900b
zhlguo <skye0515@126.com>
2020-12-06 22:47:08 +0800

    week09

---
+++ Week 09/toy-browser/server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk.toString());
+  }).on('end', () => {
+    // body = (Buffer.concat([ Buffer.from(body.toString()) ])).toString();
+    console.log('body:', body);
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('X-Foo', 'bar');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(`<html lang="en">
+
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>toy-browser</title>
+    </head>
+    
+    <style>
+      body div #myid {
+        width: 100px;
+        background-color: #ff5000;
+      }
+    
+      body div img {
+        width: 30px;
+        background-color: #ff1111;
+      }
+    </style>
+    
+    <body>
+      <div>
+        <img id="myid" />
+        <img />
+      </div>
+    </body>
+    
+    </html>
+    `);
+  })
+}).listen(8088);
+
+console.log("server started")
+ 
